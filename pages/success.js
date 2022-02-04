89c5171f30b786d393343b653f3529cecddbf88a
icorredo <71436841+icorredo@users.noreply.github.com>
2022-02-04 17:48:59 +0100

    Revert "Update success.js"
    This reverts commit 7c6132d3486012bc0d42f626b6be9a77b75fe553.

--- pages/success.js
+++ pages/success.js
@@ -1,7 +1,3 @@
-/*
-Success page shjown when the internshi submit is done
-*/
-
 import React from 'react'
 import {Heading, Box, Stack, Text} from '@chakra-ui/react'
 import {Container} from '../components'
