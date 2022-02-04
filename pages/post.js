7a3c8629c7c4313cf0297725279d97c37b4caa5e
icorredo <71436841+icorredo@users.noreply.github.com>
2022-02-04 17:12:59 +0100

    Update post.js
    [I]Menu option groups

--- pages/post.js
+++ pages/post.js
@@ -181,6 +181,25 @@ const Post = () => {
               Nanophysics
             </option>
           </Select>
+
+          <Menu closeOnSelect={false}>
+            <MenuButton as={Button} colorScheme='blue'>
+              MenuItem
+            </MenuButton>
+            <MenuList minWidth='240px'>
+              <MenuOptionGroup defaultValue='asc' title='Order' type='radio'>
+                <MenuItemOption value='asc'>Ascending</MenuItemOption>
+                <MenuItemOption value='desc'>Descending</MenuItemOption>
+              </MenuOptionGroup>
+              <MenuDivider />
+              <MenuOptionGroup title='Country' type='checkbox'>
+                <MenuItemOption value='email'>Email</MenuItemOption>
+                <MenuItemOption value='phone'>Phone</MenuItemOption>
+                <MenuItemOption value='country'>Country</MenuItemOption>
+              </MenuOptionGroup>
+            </MenuList>
+          </Menu>
+
         </FormControl>
         <FormControl id="location" isRequired mb={marginBetweenElements}>
           <FormLabel>Location</FormLabel>
